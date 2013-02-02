17f87922033ae14b0eab15b7fb1fc3b969bce6de
Marek Standio <mstandio@o2.pl>
2013-02-02 16:06:06 +0100

    StripUtils progress

--- src/main/java/org/openpano/wikieater/tools/StripUtils.java
+++ src/main/java/org/openpano/wikieater/tools/StripUtils.java
@@ -1,5 +1,8 @@
 package org.openpano.wikieater.tools;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author mstandio
  */
@@ -9,15 +12,106 @@ public class StripUtils {
 		return pageContent;
 	}
 
-	String stripDocType(String urlContent) {
-		return urlContent.substring(urlContent.indexOf(">") + 1, urlContent.length());
+	String extractDivFromPageContent(String pageContent, String divName) {
+		String patternTagOpen = "<div[^>]*?>";
+		String patternTagClose = "</div>";
+		Pattern pattern = Pattern.compile(patternTagOpen + "|" + patternTagClose, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(pageContent);
+		boolean found = false;
+		int tagCounter = 0;
+		int start = 0;
+		int end = pageContent.length();
+		while (matcher.find()) {
+			String group = matcher.group();
+			if (!found && (group.contains("id=\"" + divName + "\"") || group.contains("class=\"" + divName + "\""))) {
+				found = true;
+				tagCounter++;
+				start = matcher.start();
+			} else if (found) {
+				if (group.matches(patternTagOpen)) {
+					tagCounter++;
+				} else if (group.matches(patternTagClose)) {
+					tagCounter--;
+					if (tagCounter == 0) {
+						end = matcher.end();
+						break;
+					}
+				}
+			}
+		}
+		return pageContent.substring(start, end);
 	}
 
-	String stripNavigation(String urlContent) {
-		return null;
+	enum ElementType {
+		div, span
 	}
 
-	String stripFooter(String urlContent) {
-		return null;
+	String removeElementsFromPageContent(String pageContent, ElementType elementType, String elementName) {
+		String patternTagOpen = "<" + elementType.name() + "[^>/]*?>";
+		String patternTagClose = "</" + elementType.name() + ">";
+
+		Pattern pattern = Pattern.compile(patternTagOpen + "|" + patternTagClose, Pattern.CASE_INSENSITIVE);
+		StringBuffer stringBuffer = new StringBuffer();
+		Matcher matcher = pattern.matcher(pageContent);
+		boolean found = false;
+		int tagCounter = 0;
+		int start = 0;
+		while (matcher.find()) {
+			String group = matcher.group();
+			if (!found
+					&& (group.contains("id=\"" + elementName + "\"") || group.contains("class=\"" + elementName + "\""))) {
+				found = true;
+				stringBuffer.append(pageContent.substring(start, matcher.start()));
+				tagCounter++;
+			} else if (found) {
+				if (group.matches(patternTagOpen)) {
+					tagCounter++;
+				} else if (group.matches(patternTagClose)) {
+					tagCounter--;
+					if (tagCounter == 0) {
+						start = matcher.end();
+						found = false;
+					}
+				}
+			}
+		}
+		stringBuffer.append(pageContent.substring(start, pageContent.length()));
+		return stringBuffer.toString();
+	}
+
+	String cleanupPageContent(String pageContent) {
+		String patternTagOpen = "<[^>/]+/?>";
+		String patternTagClose = "</[^>]+>";
+
+		Pattern pattern = Pattern.compile(patternTagOpen + "|" + patternTagClose, Pattern.CASE_INSENSITIVE);
+		StringBuffer stringBuffer = new StringBuffer();
+		Matcher matcher = pattern.matcher(pageContent);
+		String typeOpening = "";
+		String typeClosing = "";
+		int previousEnd = 0;
+		int previousStart = 0;
+		int start = 0;
+		while (matcher.find()) {
+			String group = matcher.group();
+			if (group.matches(patternTagOpen)) {
+				if (group.contains(" ")) {
+					typeOpening = group.substring(1, group.indexOf(" "));
+				} else {
+					typeOpening = group.substring(1, group.indexOf(">"));
+				}
+				previousStart = matcher.start();
+				previousEnd = matcher.end();
+			} else {
+				typeClosing = group.substring(2, group.indexOf(">"));
+				if (typeOpening.equals(typeClosing) && !typeOpening.equals("script") && !typeOpening.equals("link")) {
+					if (pageContent.substring(previousEnd, matcher.start()).trim().isEmpty()) {
+						stringBuffer.append(pageContent.substring(start, previousStart));
+						start = matcher.end();
+					}
+				}
+			}
+		}
+		stringBuffer.append(pageContent.substring(start, pageContent.length()));
+		return stringBuffer.toString();
 	}
 }
--- src/test/java/org/openpano/wikieater/tools/StripUtilsTest.java
+++ src/test/java/org/openpano/wikieater/tools/StripUtilsTest.java
@@ -1,7 +1,10 @@
 package org.openpano.wikieater.tools;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.openpano.wikieater.tools.StripUtils.ElementType;
 
 /**
  * @author mstandio
@@ -16,7 +19,36 @@ public class StripUtilsTest {
 	}
 
 	@Test
-	public void stripPageContentTest() {
+	public void extractDivFromPageContent() throws Exception {
+		String pageContent = "<div>" + "<div id=\"a\"></div>" + "<div class=\"b\">" + "<div class=\"c\"></div>"
+				+ "</div>" + "<div class=\"d\"></div>" + "</div>";
+
+		String expectedResult = "<div class=\"b\">" + "<div class=\"c\"></div>" + "</div>";
+
+		assertEquals(expectedResult, stripUtils.extractDivFromPageContent(pageContent, "b"));
+	}
+
+	@Test
+	public void removeElementsFromPageContentTest() throws Exception {
+		String pageContent = "<div>" + "<div id=\"a\"></div>" + "<div class=\"a\">" + "<div class=\"b\"></div>"
+				+ "</div>" + "<div class=\"c\"></div>" + "</div>";
+
+		String expectedResult = "<div>" + "<div class=\"c\"></div>" + "</div>";
+
+		assertEquals(expectedResult, stripUtils.removeElementsFromPageContent(pageContent, ElementType.div, "a"));
+	}
+	
+	@Test
+	public void cleanupPageContent() throws Exception {
+		String pageContent = "A<h1></h1>B<h2>C</h2>D<h3 class=\"F\">  </h3>E<h4/><h4/>";
+
+		String expectedResult = "AB<h2>C</h2>DE<h4/><h4/>";
+
+		assertEquals(expectedResult, stripUtils.cleanupPageContent(pageContent));
+	}
+
+	@Test
+	public void stripPageContentTest() throws Exception {
 
 	}
 }
